774d2b25c86dad07710b7e2462f0a986a4402841
franka <f.beekman.mmh@gmail.com>
2024-07-22 11:35:33 +0200

    Fix time ago undefined

--- packages/ui/src/utils/functions.ts
+++ packages/ui/src/utils/functions.ts
@@ -34,6 +34,14 @@ dayjs.extend(relativeTime, {
     { l: 's', r: 59, d: 'second', },
     { l: 'm', r: 299, d: 'second', },
     { l: 'mm', r: 60, d: 'minute', },
+    { l: 'h', r: 1, },
+    { l: 'hh', r: 23, d: 'hour', },
+    { l: 'd', r: 1, },
+    { l: 'dd', r: 29, d: 'day', },
+    { l: 'M', r: 1, },
+    { l: 'MM', r: 11, d: 'month', },
+    { l: 'y', r: 1, },
+    { l: 'yy', d: 'year', },
   ],
 });
 dayjs.extend(updateLocale);
@@ -66,6 +74,14 @@ dayjs.updateLocale('en', {
       return `${Math.floor(number / 60)}m ${seconds}s`;
     },
     mm: '%dm',
+    h: "an hour",
+    hh: "%d hours",
+    d: "a day",
+    dd: "%d days",
+    M: "a month",
+    MM: "%d months",
+    y: "a year",
+    yy: "%d years",
   },
 });
 
