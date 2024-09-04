b9638acb8e747d3621111afb094d7efd73cbbd2b
Anastasia Tikhonova <honu347@gmail.com>
2024-09-04 11:33:53 +0200

    added pagination and sorting hook

--- apps/web/src/components/clientPages/transactions.tsx
+++ apps/web/src/components/clientPages/transactions.tsx
@@ -1,70 +1,35 @@
 'use client';
 import { FlexGrid } from '@repo/ui/atoms';
 import { SectionHeader, TableContainer } from '@repo/ui/organisms';
-import { useEffect, useState } from 'react';
 import { transactionTableHead } from '../../utils/constants.tsx';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { createTransactionRows } from '../../utils/helper.tsx';
 import { callGetTransactions } from '../../utils/api/apiCalls.tsx';
-import { TransactionType } from '../../utils/types.ts';
-import { useGatewayClientStore } from '../../store/clientStore.ts';
+import { usePaginationAndSorting } from '../../utils/hooks/usePaginationAndSorting.ts';
 
 export const Transactions = () => {
   const searchParams = useSearchParams();
-  const router = useRouter();
-  const pathname = usePathname();
 
-  const [transactions, setTransactions] = useState<TransactionType[]>([]);
-  const [totalTxs, setTotalTxs] = useState(0);
-  const [copyTooltipText, setCopyTooltipText] = useState<string>('Copy to clipboard');
-  const [loading, setLoading] = useState<boolean>(true);
-  const [pageNumber, setPageNumber] = useState<number>(Number(searchParams.get('page')) || 1);
-  const [sortField, setSortField] = useState<string>('');
-  const [sortOrder, setSortOrder] = useState<string>('');
-  const [limit, setLimit] = useState<string>(searchParams.get('limit') || '10');
-  const network = useGatewayClientStore((state) => state.network);
+  const {
+    data: transactions,
+    totalItems: totalTxs,
+    loading,
+    pageNumber,
+    limit,
+    sortField,
+    sortOrder,
+    handlePageChange,
+    handleLimitChange,
+    handleSortChange,
+  } = usePaginationAndSorting({
+    fetchFunction: callGetTransactions,
+    defaultLimit: searchParams.get('limit') || '10',
+    changeURL: true,
+  });
 
-  const updateURL = (pageNumber: number, limit: string) => {
-    router.push(pathname + '?' + `page=${pageNumber}` + `&limit=${limit}`);
-  };
+  const rows = createTransactionRows(transactions, loading, 'Copy to clipboard', () => {});
 
-  const handleSetPageNumber = async (number: number) => {
-    setPageNumber(number);
-    updateURL(number, limit);
-  };
-
-  const handlePageLimit = (value: string) => {
-    setLimit(value);
-    updateURL(pageNumber, value);
-  };
-
-  const handleSort = (field: string) => {
-    const order = sortField === field && sortOrder === 'asc' ? 'desc' : 'asc';
-    setSortField(field);
-    setSortOrder(order);
-  };
-
-  useEffect(() => {
-    setLoading(true);
-    const page = Number(searchParams.get('page')) || 1;
-    const offset = (page - 1) * Number(limit);
-    const params: any = {
-      limit,
-      offset,
-    };
-    if (sortField && sortOrder) {
-      params.sort = `${sortField}:${sortOrder}`;
-    }
-    callGetTransactions(params)
-      .then((data) => {
-        setTotalTxs(data.meta.total);
-        setTransactions(data.data);
-      })
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, [searchParams, sortField, sortOrder, network, limit]);
-
-  const rows = createTransactionRows(transactions, loading, copyTooltipText, setCopyTooltipText);
+  const totalPages = Math.ceil(totalTxs / Number(limit));
 
   return (
     <FlexGrid className="w-full gap-9 desktop:gap-12 mx-auto" direction={'col'}>
@@ -76,13 +41,13 @@ export const Transactions = () => {
       <TableContainer
         currentNumber={pageNumber}
         defaultValue={limit}
-        headCols={transactionTableHead(handleSort, sortField, sortOrder)}
+        headCols={transactionTableHead(handleSortChange, sortField, sortOrder)}
         keyPrefix={'transactions'}
-        onPerPageChange={handlePageLimit}
+        onPerPageChange={handleLimitChange}
         pagination
         rows={rows}
-        setCurrentNumber={handleSetPageNumber}
-        totalPages={Math.ceil(totalTxs / Number(limit))}
+        setCurrentNumber={handlePageChange}
+        totalPages={totalPages}
       />
     </FlexGrid>
   );
