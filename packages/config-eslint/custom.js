f52010e37f4fd7481d7a0d70795e9106ca86aaad
franka <f.beekman.mmh@gmail.com>
2024-09-09 11:03:50 +0200

    change fix to eslint error

--- packages/config-eslint/custom.js
+++ packages/config-eslint/custom.js
@@ -1,6 +1,6 @@
 module.exports = {
-  env: {
-    es2020: true,
+  globals: {
+    BigInt: true,
   },
   ignorePatterns: ['**/*.stories.tsx'],
   extends: ['eslint:recommended', 'next', 'turbo', 'prettier'],
