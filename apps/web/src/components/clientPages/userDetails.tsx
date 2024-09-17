19a26fe6a1ff0bb97bca42cdb66e9a46dc8271f7
Anastasia Tikhonova <honu347@gmail.com>
2024-09-17 10:01:28 +0200

    🌱 User Details | Add tabs

--- apps/web/src/components/clientPages/userDetails.tsx
+++ apps/web/src/components/clientPages/userDetails.tsx
@@ -1,8 +1,45 @@
+'use client';
+
+import { FlexGrid, TabButtons } from '@repo/ui/atoms';
+// import { DetailsSection, SectionHeader, TableContainer } from '@repo/ui/organisms';
+
 export const UserDetails = ({ params }: { params: { id: string } }) => {
+  const tabs = [
+    {
+      value: 1,
+      label: 'Details',
+      icon: 'InfoSquare',
+      content: (
+        <>1</>
+        // <DetailsSection
+        //   data={}
+        //   json={}
+        //   title={'User Details'}
+        // />
+      ),
+    },
+    {
+      value: 2,
+      label: 'Transactions',
+      icon: 'SwitchHorizontal',
+      content: <>2</>,
+    },
+    {
+      value: 3,
+      label: 'Stakes',
+      icon: 'LayersThree',
+      content: <>3</>,
+    },
+    {
+      value: 4,
+      label: 'Events',
+      icon: 'List',
+      content: <>4</>,
+    },
+  ];
   return (
-    <div>
-      <h1>User Details</h1>
-      <p>User ID: {params.id}</p>
-    </div>
+    <FlexGrid direction={'col'} gap={'5xl'}>
+      <TabButtons tabs={tabs} />
+    </FlexGrid>
   );
 };
