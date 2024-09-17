5eb840580dbb00a2750b5832927f751af03c333a
Anastasia Tikhonova <honu347@gmail.com>
2024-09-17 10:27:20 +0200

    🌱 User details | implement data

--- apps/web/src/components/clientPages/userDetails.tsx
+++ apps/web/src/components/clientPages/userDetails.tsx
@@ -1,21 +1,55 @@
 'use client';
 
-import { FlexGrid, TabButtons } from '@repo/ui/atoms';
-// import { DetailsSection, SectionHeader, TableContainer } from '@repo/ui/organisms';
+import { FlexGrid, TabButtons, Typography, CopyIcon } from '@repo/ui/atoms';
+import { DetailsSection, SectionHeader, TableContainer } from '@repo/ui/organisms';
+import { UsersType } from '../../utils/types.ts';
+import { useState, useEffect } from 'react';
+import { DataType } from '@repo/ui/types';
+import { callGetValidators } from '../../utils/api/apiCalls.tsx';
 
 export const UserDetails = ({ params }: { params: { id: string } }) => {
+  const [user, setUser] = useState<UsersType | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+    callGetValidators({
+      address: params.id,
+    })
+      .then((data) => setUser(data.data[0]))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, [params.id]);
+
+  const createDetails = (label: string, value: any = ' - ', mobileWidth?: string) => {
+    return { label: { label }, value, mobileWidth };
+  };
+
+  const details = [
+    createDetails(
+      'Validator address',
+      <div className="flex flex-row gap-1.5 items-baseline ">
+        <Typography variant={'paragraph-sm'}>{user?.account.address}</Typography>
+        <CopyIcon content={user?.account.address || ''} size={'xxs'} />
+      </div>,
+    ),
+    createDetails(
+      'Public Key',
+      <div className="flex flex-row gap-1.5 items-baseline ">
+        <Typography variant={'paragraph-sm'}>{user?.account.publicKey}</Typography>
+        <CopyIcon content={user?.account.publicKey || ''} size={'xxs'} />
+      </div>,
+      'half',
+    ),
+  ];
+
   const tabs = [
     {
       value: 1,
       label: 'Details',
       icon: 'InfoSquare',
       content: (
-        <>1</>
-        // <DetailsSection
-        //   data={}
-        //   json={}
-        //   title={'User Details'}
-        // />
+        <DetailsSection data={details} json={user as unknown as DataType} title={'User Details'} />
       ),
     },
     {
@@ -37,6 +71,7 @@ export const UserDetails = ({ params }: { params: { id: string } }) => {
       content: <>4</>,
     },
   ];
+
   return (
     <FlexGrid direction={'col'} gap={'5xl'}>
       <TabButtons tabs={tabs} />
