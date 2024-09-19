09bf6e8eef7082c25e94320c8d2728eaf569ba67
Anastasia Tikhonova <honu347@gmail.com>
2024-09-19 11:13:04 +0200

    ♻️ Tooltip | Hide on small screen

--- packages/ui/src/components/atoms/utilities/tooltip.tsx
+++ packages/ui/src/components/atoms/utilities/tooltip.tsx
@@ -1,89 +1,100 @@
-"use client"
-import {Popper} from "@mui/base";
-import {CSSProperties, ReactNode, useState} from "react";
-import {Typography} from "../base/typography.tsx";
-import {cls} from "../../../utils/functions.ts";
+'use client';
+import { Popper } from '@mui/base';
+import { CSSProperties, ReactNode, useState } from 'react';
+import { Typography } from '../base/typography.tsx';
+import { cls } from '../../../utils/functions.ts';
 
 interface TooltipProps {
-  placement: "top" | "bottom" | "left" | "right";
+  placement: 'top' | 'bottom' | 'left' | 'right';
   text: string;
   children: ReactNode;
 }
 
-export const Tooltip = ({ placement, text, children, }: TooltipProps) => {
+export const Tooltip = ({ placement, text, children }: TooltipProps) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [open, setOpen] = useState(false);
   const arrowSize = 4;
 
-  let positionStyles: CSSProperties =  {
+  let positionStyles: CSSProperties = {
     left: 0,
     right: 0,
     bottom: `-${arrowSize}px`,
-  }
+  };
 
   switch (placement) {
-    case "top":
+    case 'top':
       positionStyles = {
         left: 0,
         right: 0,
         bottom: `-${arrowSize}px`,
-      }
+      };
       break;
-    case "bottom":
+    case 'bottom':
       positionStyles = {
         left: 0,
         right: 0,
         top: `-${arrowSize}px`,
-        transform: "rotate(180deg)",
-      }
+        transform: 'rotate(180deg)',
+      };
       break;
-    case "left":
+    case 'left':
       positionStyles = {
         right: `-${arrowSize + 2}px`,
         top: 0,
         bottom: 0,
-        transform: "rotate(270deg)",
-      }
+        transform: 'rotate(270deg)',
+      };
       break;
-    case "right":
+    case 'right':
       positionStyles = {
         left: `-${arrowSize + 2}px`,
         top: 0,
         bottom: 0,
-        transform: "rotate(90deg)",
-      }
+        transform: 'rotate(90deg)',
+      };
       break;
   }
 
   return (
-    <div onMouseEnter={() => setOpen(true)} onMouseLeave={() => setOpen(false)} ref={setAnchorEl}>
+    <div
+      className="hidden desktop:flex"
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      ref={setAnchorEl}
+    >
       {children}
       <Popper
         anchorEl={anchorEl}
         className={cls([
-            placement === "top" && "pb-lg",
-            placement === "bottom" && "pt-lg",
-            placement === "left" && "pr-lg",
-            placement === "right" && "pl-lg",
+          placement === 'top' && 'pb-lg',
+          placement === 'bottom' && 'pt-lg',
+          placement === 'left' && 'pr-lg',
+          placement === 'right' && 'pl-lg',
         ])}
         open={open}
         placement={placement}
       >
-        <div className={"relative bg-background rounded-sm px-lg py-sm shadow-below shadow-shadow-gray-3 z-10 box-border"}>
-          <Typography fontWeight={"semibold"} variant={"caption"}>{text}</Typography>
+        <div
+          className={
+            'relative bg-background rounded-sm px-lg py-sm shadow-below shadow-shadow-gray-3 z-10 box-border'
+          }
+        >
+          <Typography fontWeight={'semibold'} variant={'caption'}>
+            {text}
+          </Typography>
           <div
-            className={"absolute m-auto"}
+            className={'absolute m-auto'}
             style={{
-              width: "0",
-              height: "0",
-              borderStyle: "solid",
+              width: '0',
+              height: '0',
+              borderStyle: 'solid',
               borderWidth: `${arrowSize}px ${arrowSize}px 0 ${arrowSize}px`,
-              borderColor: "var(--color-background) transparent transparent transparent",
+              borderColor: 'var(--color-background) transparent transparent transparent',
               ...positionStyles,
             }}
           />
         </div>
       </Popper>
     </div>
-  )
-}
+  );
+};
