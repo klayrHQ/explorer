6590c47090f8bbd6848e78d7593396ccfdbdf5a6
franka <f.beekman.mmh@gmail.com>
2024-09-30 15:53:40 +0200

    :recycle: Account | post-merge cleanup

--- apps/web/src/app/(user)/[chain]/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AccountDetails } from '../../../../../components/clientPages/accountDetails.tsx';
-
-export default function Page({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <AccountDetails paramAccount={params.id} />
-    </div>
-  );
-}
