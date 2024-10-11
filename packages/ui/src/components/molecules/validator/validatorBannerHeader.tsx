bac5d815913c7a08546da1e0ca4ca66dac295cae
Anastasia Tikhonova <honu347@gmail.com>
2024-10-11 14:09:03 +0200

    ♻️ Validator banner | mobile status should be the same as the desktop

--- packages/ui/src/components/molecules/validator/validatorBannerHeader.tsx
+++ packages/ui/src/components/molecules/validator/validatorBannerHeader.tsx
@@ -34,22 +34,8 @@ export const ValidatorBannerHeader = ({
           size={40}
           width="auto"
         />
-        <div className="hidden desktop:flex mb-4">
+        <div className=" desktop:flex mb-4">
           <StatusBadge status={status || ''} />
-        </div>
-        <div className="flex items-center desktop:hidden mb-4">
-          {status === 'active' ? (
-            <div className=" flex items-center justify-center w-5 h-5 mr-0.5 rounded-sm border-1 border-success shadow-sm">
-              <BadgeIcon colorVariant="success" height="1.5" width="1.5" />
-            </div>
-          ) : (
-            <div>
-              <div className=" flex items-center justify-center w-5 h-5 rounded-sm border-1 border-error shadow-sm">
-                <BadgeIcon colorVariant="error" height="1.5" width="1.5" />
-              </div>
-            </div>
-          )}
-          {/* <ValidatorBannerButtons /> */}
         </div>
       </div>
     </div>
