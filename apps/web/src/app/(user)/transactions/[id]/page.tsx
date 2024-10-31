fa0fd061f6de1a6e64e3fb38b1db1f3956953b7d
Franka <f.beekman.mmh@gmail.com>
2024-10-31 10:23:15 +0100

    :bug: Routing | Delete top level pages

--- apps/web/src/app/(user)/transactions/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TransactionDetails } from "../../../../components/clientPages/transactionDetails.tsx";
-
-export default function Page({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <TransactionDetails params={{ id: params.id }} />
-    </div>
-  );
-}
