cf350fa155c97a08e18b9d168530ae21f14a3b56
franka <f.beekman.mmh@gmail.com>
2024-11-18 15:48:13 +0100

    :recycle: Chain Details | Add live data

--- apps/web/src/components/clientPages/chainDetails.tsx
+++ apps/web/src/components/clientPages/chainDetails.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FlexGrid } from '@repo/ui/atoms';
+import { FlexGrid, ImageContainer, Link, StatusBadge } from '@repo/ui/atoms';
 import { DetailsSection } from '@repo/ui/organisms';
 import { createDetails } from '../../utils/helpers/dataHelpers';
 import BannerBG from '../../assets/images/bannerBG.png';
@@ -7,57 +7,122 @@ import { Typography } from '@repo/ui/atoms';
 import { ChainDetailsBanner } from '@repo/ui/organisms';
 import { useChainNetworkStore } from '../../store/chainNetworkStore.ts';
 import { FormattedValue } from '../formattedValue.tsx';
+import Placeholder from '../../assets/images/placeholder.png';
 
 export const ChainDetails = ({ params }: { params: { id: string } }) => {
   const chains = useChainNetworkStore((state) => state.chains);
-  const mockChainData = chains?.[0];
+  const chain = chains?.find((chain) => chain.chainID === params.id);
+
+  const serviceURLDetails = chain?.serviceURLs
+    ? chain?.serviceURLs?.map((serviceURL) => {
+        return (
+          createDetails(
+            'http',
+            <FormattedValue
+              value={serviceURL.http}
+              format={'string'}
+              copy
+              typographyProps={{ color: 'onBackgroundHigh' }}
+            />,
+          ),
+          createDetails(
+            'ws',
+            <FormattedValue
+              value={serviceURL.ws}
+              format={'string'}
+              copy
+              typographyProps={{ color: 'onBackgroundHigh' }}
+            />,
+          )
+        );
+      })
+    : [];
 
   const details = [
     createDetails(
+      'Chain ID',
+      <FormattedValue
+        format={'string'}
+        value={chain?.chainID ?? ''}
+        copy
+        typographyProps={{ color: 'onBackgroundHigh' }}
+      />,
+    ),
+    createDetails(
+      'Chain Name',
+      <Typography variant={'paragraph-sm'}>{chain?.chainName}</Typography>,
+    ),
+    createDetails(
+      'Display Name',
+      <Typography variant={'paragraph-sm'}>{chain?.displayName}</Typography>,
+    ),
+    createDetails(
       'Description',
-      <Typography variant={'paragraph-sm'}>{'Here is the description of the chain'}</Typography>,
+      <Typography variant={'paragraph-sm'}>{chain?.description ?? '-'}</Typography>,
     ),
     createDetails(
-      'Block time',
-      <Typography variant={'paragraph-sm'}>{'block time'}</Typography>,
-      'half',
-    ),
-    createDetails(
-      'Height',
-      <FormattedValue
-        format={'number'}
-        typographyProps={{ color: 'onBackgroundHigh' }}
-        value={213423345}
+      'Chain Icon',
+      <ImageContainer
+        alt={chain?.displayName ?? chain?.chainName ?? ''}
+        src={chain?.logo.png ?? chain?.logo.svg ?? Placeholder.src}
+        variant={'avatar'}
       />,
     ),
+    createDetails('Status', <StatusBadge status={chain?.status ?? 'inactive'} />),
     createDetails(
-      'Finalized height',
-      <FormattedValue
-        format={'number'}
-        typographyProps={{ color: 'onBackgroundHigh' }}
-        value={256234123}
-      />,
+      'Network Type',
+      <Typography variant={'paragraph-sm'}>{chain?.networkType}</Typography>,
     ),
     createDetails(
-      'Blocks until finalized',
-      <FormattedValue
-        format={'number'}
-        typographyProps={{ color: 'onBackgroundHigh' }}
-        value={2453}
-      />,
+      'Project Page',
+      chain?.projectPage ? (
+        <Link href={chain?.projectPage} outgoing>
+          <Typography className={'underline'} variant={'paragraph-sm'} link>
+            {chain?.projectPage}
+          </Typography>
+        </Link>
+      ) : (
+        <Typography variant={'paragraph-sm'}>{'-'}</Typography>
+      ),
     ),
-    createDetails('Network version', <Typography variant={'paragraph-sm'}>{'3.0'}</Typography>),
-    createDetails('Network identifier', <Typography variant={'paragraph-sm'}>{'KLY'}</Typography>),
-    createDetails('Max payload length', <Typography variant={'paragraph-sm'}>{'7'}</Typography>),
+    createDetails(
+      'Genesis URL',
+      chain?.genesisURL ? (
+        <Link href={chain?.genesisURL} outgoing>
+          <Typography className={'underline'} variant={'paragraph-sm'} link>
+            {chain?.genesisURL}
+          </Typography>
+        </Link>
+      ) : (
+        <Typography variant={'paragraph-sm'}>{'-'}</Typography>
+      ),
+    ),
+    createDetails(
+      'Service URLs',
+      <FlexGrid direction={'col'} gap={'md'}>
+        <FormattedValue
+          value={chain?.serviceURLs[0]?.http}
+          format={'string'}
+          copy
+          typographyProps={{ color: 'onBackgroundHigh' }}
+        />
+        <FormattedValue
+          value={chain?.serviceURLs[0]?.ws}
+          format={'string'}
+          copy
+          typographyProps={{ color: 'onBackgroundHigh' }}
+        />
+      </FlexGrid>,
+    ),
   ];
 
   return (
     <FlexGrid direction={'col'} gap={'5xl'}>
       <ChainDetailsBanner
-        chain={mockChainData}
+        chain={chain}
         image={BannerBG.src}
         locked={0}
-        logo={mockChainData?.logo.png ?? ''}
+        logo={chain?.logo.png ?? chain?.logo.svg ?? Placeholder.src}
         status={'Active'}
       />
       <DetailsSection data={details} title="Chain details" />
