e8c02fdd3e5a06d3e964960fe00c64ae49a270dc
franka <f.beekman.mmh@gmail.com>
2024-12-02 13:51:33 +0100

    :recycle: Snackbar | make background opaque

--- packages/ui/src/components/atoms/notifications/snackbarBody.tsx
+++ packages/ui/src/components/atoms/notifications/snackbarBody.tsx
@@ -40,30 +40,32 @@ export const SnackbarBody = forwardRef(
   ({ title, text, handleClose, variant }: SnackbarProps, ref: Ref<HTMLDivElement>) => {
     const { style, icon } = SnackbarVariant[variant];
     return (
-      <div className={`w-96 ${style} rounded-lg border-1 relative`} ref={ref}>
-        <div className="absolute top-2 right-2">
-          <IconButton
-            className="w-3 h-3 text-onBackground"
-            icon="CrossClose"
-            onClick={handleClose}
-            variant="transparent"
-          />
-        </div>
-        <div className="flex gap-1.5 ">
-          <div className="mt-4 ml-1.5">
-            <Icon icon={icon} />
+      <div className={'bg-backgroundPrimary'} ref={ref}>
+        <div className={`w-96 ${style} rounded-lg border-1 relative`}>
+          <div className="absolute top-2 right-2">
+            <IconButton
+              className="w-3 h-3 text-onBackground"
+              icon="CrossClose"
+              onClick={handleClose}
+              variant="transparent"
+            />
           </div>
-          <div className="flex flex-col my-6 gap-2 max-w-72">
-            <Typography color="text-onBackground" fontWeight="bold" variant="paragraph-md">
-              {title}
-            </Typography>
-            <Typography
-              className="text-onBackground"
-              color="onBackgroundMedium"
-              variant="paragraph-sm"
-            >
-              {text}
-            </Typography>
+          <div className="flex gap-1.5 ">
+            <div className="mt-4 ml-1.5">
+              <Icon icon={icon} />
+            </div>
+            <div className="flex flex-col my-6 gap-2 max-w-72">
+              <Typography color="text-onBackground" fontWeight="bold" variant="paragraph-md">
+                {title}
+              </Typography>
+              <Typography
+                className="text-onBackground"
+                color="onBackgroundMedium"
+                variant="paragraph-sm"
+              >
+                {text}
+              </Typography>
+            </div>
           </div>
         </div>
       </div>
