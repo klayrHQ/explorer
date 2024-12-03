f612b8203b54c72702a8194b2469b982cf6737b3
Franka <f.beekman.mmh@gmail.com>
2024-12-03 12:57:54 +0100

    :recycle: Vercel | test 4 stop 404 on previews

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -18,13 +18,10 @@ export function middleware(req: NextRequest) {
     return NextResponse.next();
   }
 
-  // Redirect for Vercel previews: mainchain and testnet by default, rest has to be done manually
+  // todo add permanent fix for vercel previews
+  // Temporarily skip the redirect for Vercel previews
   if (hostname.split('.')[1] === 'vercel') {
-    if (url.searchParams.entries.length > 0) return NextResponse.next();
-    url.searchParams.set('network', 'testnet');
-    url.searchParams.set('app', 'klayr_mainchain');
-
-    return NextResponse.redirect(url);
+    return NextResponse.next();
   }
 
   // Set the default searchParams if the subdomain is not explorer or testnet-explorer (mainly for localhost)
