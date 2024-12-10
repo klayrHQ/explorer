4582bb99e084321770847a252706eaa6930bebc3
Anastasia Tikhonova <honu347@gmail.com>
2024-12-10 15:51:06 +0100

    ♻️  Fix typo

--- apps/web/src/components/clientPages/blockDetails.tsx
+++ apps/web/src/components/clientPages/blockDetails.tsx
@@ -304,8 +304,8 @@ export const BlockDetails = ({ params }: { params: { id: string } }) => {
           ) : (
             <NotFound
               className="mt-16"
-              headerText={'No transactions found'}
-              subheaderText={'We cannot find any transactions in this block'}
+              headerText={'No events found'}
+              subheaderText={'We cannot find any events in this block'}
             />
           )}
         </FlexGrid>
