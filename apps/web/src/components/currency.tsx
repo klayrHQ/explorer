e5d7b44569190d00e946bbfac5d1d02defbe98e8
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-09-26 07:00:44 +0700

    change default currency sign to K and add space

--- apps/web/src/components/currency.tsx
+++ apps/web/src/components/currency.tsx
@@ -4,20 +4,17 @@ import { useChainNetworkStore } from '../store/chainNetworkStore.ts';
 import { useSettingsStore } from '../store/settingsStore.ts';
 
 export const Currency = ({ symbol, decimals, ...props }: CurrencyProps) => {
-  const currentChain = useChainNetworkStore((state) => state.currentChain);
   const currentChainToken = useChainNetworkStore((state) => state.currentChainToken);
   const currency = currentChainToken?.symbol;
   const {
     currency: { mantissaSize, trailingZeroes, decimalSeparator, formatting },
   } = useSettingsStore((state) => state.settings);
 
-  const isMainChain = currentChain?.chainName === 'klayr_mainchain';
-
   return (
     <UICurrency
       {...props}
       symbol={formatting.includes('Symbol') ? (symbol ?? currency ?? 'KLY') : undefined}
-      sign={formatting.includes('Sign') && isMainChain ? 'Ҝ' : undefined}
+      sign={formatting.includes('Sign') ? (props.sign ?? 'Ҝ') : undefined}
       decimals={decimals ?? Number(mantissaSize)}
       trailingZeroes={trailingZeroes}
       separator={decimalSeparator as 'Comma' | 'Period'}
