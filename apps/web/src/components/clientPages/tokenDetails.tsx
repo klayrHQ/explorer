fa3a2819f794a11f0e24dd390f23e718eb74f979
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-09-27 17:28:01 +0700

    add arrow back to token data details

--- apps/web/src/components/clientPages/tokenDetails.tsx
+++ apps/web/src/components/clientPages/tokenDetails.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { useChainNetworkStore } from '../../store/chainNetworkStore';
-import { FlexGrid, TabButtons, BannerFrame, Typography } from '@repo/ui/atoms';
+import {
+  FlexGrid,
+  TabButtons,
+  BannerFrame,
+  Typography,
+  Icon,
+  ImageContainer,
+} from '@repo/ui/atoms';
 import { ImageName } from '@repo/ui/molecules';
 import BannerBG from '../../assets/images/bannerBG.png';
 import { DetailsSection } from '@repo/ui/organisms';
+
 export const TokenDetails = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
   const token = useChainNetworkStore((state) => state.tokens).find(
     (token) => token.tokenID === params.id,
   );
@@ -62,9 +72,15 @@ export const TokenDetails = ({ params }: { params: { id: string } }) => {
     <FlexGrid direction={'col'} gap={'5xl'}>
       <BannerFrame image={BannerBG}>
         <div className="flex items-center gap-2 ">
+          <Icon
+            onClick={() => router.back()}
+            className="hover:-translate-x-0.5 cursor-pointer transition-transform"
+            color="white"
+            icon="ArrowLeft"
+          />
           <div className="w-10 h-10 aspect-square rounded-full">
             {tokenWithChainData?.logo?.png && (
-              <img
+              <ImageContainer
                 src={tokenWithChainData.logo.png}
                 alt={tokenWithChainData.tokenName ?? 'Token logo'}
                 className="w-full h-full object-cover rounded-full"
