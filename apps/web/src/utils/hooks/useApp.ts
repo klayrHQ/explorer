2930450e805ad495e73486fa46b7ea5f4068dfe7
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-11-04 21:02:52 +0700

    changes to app and/or network should trigger re-render

--- apps/web/src/utils/hooks/useApp.ts
+++ apps/web/src/utils/hooks/useApp.ts
@@ -1,13 +1,13 @@
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const useApp = () => {
+  const pathname = usePathname();
   const searchParams = useSearchParams();
+
   if (searchParams.get('app')) return searchParams.get('app');
 
-  if (typeof window !== 'undefined') {
-    const pathSegments = window.location.pathname.split('/').filter(Boolean);
-    if (pathSegments[0]) return pathSegments[0];
-  }
+  const pathSegments = pathname.split('/').filter(Boolean);
+  if (pathSegments[0]) return pathSegments[0];
 
   return 'klayr_mainchain';
 };
--- apps/web/src/utils/hooks/useNetwork.ts
+++ apps/web/src/utils/hooks/useNetwork.ts
@@ -1,17 +1,17 @@
 import { useSearchParams } from 'next/navigation';
+import { useHostname } from './useHostname';
 
 const DEFAULT_NETWORK = 'mainnet';
 
 export const useNetwork = () => {
   const searchParams = useSearchParams();
+  const hostname = useHostname();
+
   if (searchParams.get('network')) return searchParams.get('network');
 
-  if (typeof window !== 'undefined') {
-    const subdomain = window.location.hostname.split('.')[0];
-
-    if (subdomain && subdomain.endsWith('-explorer')) {
-      return subdomain.split('-explorer')[0];
-    }
+  const subdomain = hostname.split('.')[0];
+  if (subdomain && subdomain.endsWith('-explorer')) {
+    return subdomain.split('-explorer')[0];
   }
 
   return DEFAULT_NETWORK;
