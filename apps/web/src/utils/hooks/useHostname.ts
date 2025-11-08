3ec3a20e8b9e03d76d7d0520e714c899b08cf3ca
Aldo Suhartono Putra <aldhosutra@gmail.com>
2025-11-08 20:47:20 +0700

    fix useInsertionEffect warning in useHostname

--- apps/web/src/utils/hooks/useHostname.ts
+++ apps/web/src/utils/hooks/useHostname.ts
@@ -1,34 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export function useHostname() {
-  const [hostname, setHostname] = useState(
-    typeof window !== 'undefined' ? window.location.hostname : '',
-  );
-
-  useEffect(() => {
-    const updateHostname = () => setHostname(window.location.hostname);
-
-    window.addEventListener('popstate', updateHostname);
-
-    const originalPushState = history.pushState;
-    const originalReplaceState = history.replaceState;
-
-    history.pushState = function (...args) {
-      originalPushState.apply(this, args);
-      updateHostname();
-    };
-
-    history.replaceState = function (...args) {
-      originalReplaceState.apply(this, args);
-      updateHostname();
-    };
-
-    return () => {
-      window.removeEventListener('popstate', updateHostname);
-      history.pushState = originalPushState;
-      history.replaceState = originalReplaceState;
-    };
-  }, []);
-
+  const [hostname] = useState(typeof window !== 'undefined' ? window.location.hostname : '');
   return hostname;
 }
